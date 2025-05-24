7a28ca98595d766c64d7b45e272f1ba25852bb56
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-25 00:52:46 +0500

    clonning Asim Jofa Website

---
+++ app.js
@@ -0,0 +1,9 @@
+// let navbar = document.getElementsByTagName("nav")[0];
+// console.log(navbar);
+
+
+// document.onscroll = ()=> {
+//         navbar.style.position = "fixed"
+//         navbar.style.top = "0"
+//         navbar.style.left = "0"
+// }
